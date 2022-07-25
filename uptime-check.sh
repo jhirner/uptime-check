f59e40dc4711caa8103e42b810b389f39cbcea67
Josh Hirner <68080353+jhirner@users.noreply.github.com>
2022-07-24 17:14:45 -0700

    Create uptime-check.sh
    Initial commit of bash script to check network status via ping.

---
+++ uptime-check.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+#
+### Uptime Check:
+### A simple script to monitor a home web server for connectivity
+### interruptions. If the network is lost, reboot.
+### Intended to be run on a regular schedule.
+### https://github.com/jhirner/uptime-check
+### Distrubuted under the GPL-3.0 license.
+#
+
+# Get the IP address
+ip=$(curl --silent ifconfig.me)
+
+# Get % of packets lost in 5 replicate pings.
+# 'lossstr' is a string in the format of "0% packet loss", and
+# 'losspct' is simply the numerical portion of lossstr.
+lossstr=$(ping -c 5 $ip -O |  grep 'packet loss' | awk -F ',' '{print $(NF-1)}')
+losspct=$(echo "$lossstr" | awk -F '%' '{print $1}')
+
+if [ $losspct -gt 0 ]; then
+     echo "Check PASSED. $losspct %  packet loss."
+else
+     echo "Check FAILED. $losspct %  packet loss. Rebooting."
+     /usr/sbin/reboot
+fi
